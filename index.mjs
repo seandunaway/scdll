2d48213a24b8482296a4078d0b2cd73baea8a045
seandunaway <git@seandunaway.com>
2024-08-12 22:09:40 -0700

    init

---
+++ index.mjs
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import {createSocket} from 'node:dgram'
+import {parseArgs} from 'node:util'
+import {exit} from 'node:process'
+
+let args = parseArgs({
+	options: {
+		address: {short: 'a', type: 'string', default: 'localhost'},
+		port: {short: 'p', type: 'string', default: '22903'}
+	},
+	allowPositionals: true,
+})
+let address = args.values.address
+let port = parseInt(/** @type {string} */ (args.values.port))
+let command = args.positionals[0]
+
+let message = ''
+switch (command) {
+	case 'deny':
+		message = 'RELEASE_ALL_DLLS'
+		break
+	case 'allow':
+		message = 'ALLOW_LOAD_ALL_DLLS'
+		break
+	default:
+		console.info('usage: [-p <port>] <deny | allow>')
+		exit(1)
+}
+
+let socket = createSocket('udp4')
+socket.send(message, port, address, function (error) {
+	if (error) throw error
+	socket.close()
+})
